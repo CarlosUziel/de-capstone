b599aaecf9ffc4585cec882f0ce892ad71c2c40d
cauzpema@gmail.com <cauzpema@gmail.com>
2022-12-09 20:24:35 +0100

    add spark utils functions

---
+++ src/utils/spark.py
@@ -0,0 +1,34 @@
+from configparser import ConfigParser
+
+import pyspark
+from pyspark.sql import SparkSession
+
+
+def create_spark_session(user_config: ConfigParser, dl_config: ConfigParser):
+    """
+    Create spark session
+
+    Args:
+        user_config: user configuration parameters.
+        dl_config: data lake configuration parameters.
+    """
+    # 1. Create Spark session with S3 access
+    spark = (
+        SparkSession.builder.appName("Sparkify")
+        .config(
+            "spark.jars.packages", f"org.apache.hadoop:hadoop-aws:{pyspark.__version__}"
+        )
+        .getOrCreate()
+    )
+
+    # 2. Set S3 parameters
+    hadoop_conf = spark.sparkContext._jsc.hadoopConfiguration()
+    hadoop_conf.set(
+        "fs.s3a.endpoint", f"s3-{dl_config.get('GENERAL', 'REGION')}.amazonaws.com"
+    )
+    hadoop_conf.set("fs.s3a.access.key", user_config.get("AWS", "AWS_ACCESS_KEY_ID"))
+    hadoop_conf.set(
+        "fs.s3a.secret.key", user_config.get("AWS", "AWS_SECRET_ACCESS_KEY")
+    )
+
+    return spark
